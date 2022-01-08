f5e580de788308f1ee84bddff809162f9ce39578
pablogonz <65257437+pablogonz@users.noreply.github.com>
2022-01-07 20:10:02 -0400

    main

---
+++ katainfo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace katainfo
+{
+    public class information
+    {
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public int Edad { get; set; }
+        public information(string nombre, string apellido, int edad)
+        {
+
+            Nombre = nombre;
+            Apellido = apellido;
+            Edad = edad;
+        }
+        public static void Main()
+        {
+
+        }
+    }
+}
